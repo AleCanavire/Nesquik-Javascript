fce6dc8735f5c32e13a4c0e682fcbee21e9aef4c
AleCanavire <ale.canavire1@gmail.com>
2022-09-22 17:44:43 -0300

    Pre-entrega

---
+++ js/index.js
@@ -0,0 +1,51 @@
+// Validacion de texto en el input
+const indexInput = document.querySelector('#email');
+const emailSpan = document.querySelector('.placelabel');
+
+indexInput.addEventListener('keyup', () => {
+    if (indexInput.value != "") {
+        emailSpan.classList.add('focusInput');
+    } else{
+        emailSpan.classList.remove('focusInput');
+    }
+});
+
+// Tomar el email para rellenarlo automaticamente al signup
+indexInput.addEventListener('keyup', () => {
+    localStorage.setItem('email', indexInput.value);
+});
+
+// Cambio de idioma 
+const selectLang = document.querySelector('#selectLang');
+
+const signup = document.querySelector('#signup');
+const title = document.querySelector('#title');
+const newAccount = document.querySelector('#newAccount');
+const start = document.querySelector('#start');
+const background = document.querySelector('#background');
+
+selectLang.addEventListener("change", () => {
+    if (selectLang.value == "Español") {
+        signup.innerText = "Iniciar sesión";
+        title.innerHTML = `
+        <h1>Películas y series ilimitadas y mucho más.</h1>
+        <h2>Disfruta donde quieras. Cancela cuando quieras.</h2>`;
+        newAccount.innerText = "¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o reiniciar tu membresía de Netflix."
+        emailSpan.innerText = "Email"
+        start.innerText = "Comenzar";
+        background.innerHTML = `
+        <img src="img/background-es.jpg" alt="background movies">
+        `
+    } else if(selectLang.value == "English"){
+        signup.innerText = "Sign In";
+        title.innerHTML = `
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>`
+        newAccount.innerText = "Ready to watch? Enter your email to create or restart your membership."
+        emailSpan.innerText = "Email Address"
+        start.innerText = "Get Started";
+        background.innerHTML = `
+        <img src="img/background-en.jpg" alt="background movies">
+        `
+    }
+})
---
+++ js/signupOne.js
@@ -0,0 +1,52 @@
+// Validacion de texto en el input
+const inputPassword = document.getElementById('password');
+const passwordSpan = document.querySelector('.password-label');
+
+inputPassword.addEventListener('keyup', () => {
+    if (inputPassword.value != "") {
+        passwordSpan.classList.add('focusInput');
+    } else{
+        passwordSpan.classList.remove('focusInput');
+    }
+});
+
+// Rellenar automaticamente el email con el email ingresado en el index
+const userEmail = document.querySelector('.user-email');
+userEmail.innerText = localStorage.getItem('email');
+
+// Guardar password en el localStorage
+inputPassword.addEventListener('keyup', () => {
+    localStorage.setItem('password', inputPassword.value);
+});
+
+// Crear nuevo usuario
+const btnNewUser = document.querySelector('#newUser-btn');
+btnNewUser.addEventListener("click", () =>{
+    newUser()
+})
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+})
+
+// Crear nuevo usuario funcion
+function newUser() {
+    const user = {
+        email: localStorage.getItem('email') ,
+        pass: inputPassword.value
+    }
+
+    let users = JSON.parse(localStorage.getItem('users'));
+    if (users) {
+        users.push(user);
+        let usersJSON = JSON.stringify(users);
+        localStorage.setItem('users', usersJSON);
+    } else {
+        let users = [];
+        users.push(user);
+        let usersJSON = JSON.stringify(users);
+        localStorage.setItem('users', usersJSON);
+    }
+}
