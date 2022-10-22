7d59c81ec1d15200dcb5048f91bbf0d2c6bf4dcf
AleCanavire <ale.canavire1@gmail.com>
2022-10-22 14:05:44 -0300

    latestSlider

--- js/home.js
+++ js/home.js
@@ -129,6 +129,58 @@ account.addEventListener('mouseover', ()=>{
 
 
 // ==================== HERO ====================
+// ---------- GET INFO HERO ----------
+const mainInfo = async(type, id) => {
+    const logoInfo = document.getElementById('logoInfo');
+    const logoMain = await getLogos(type, id);
+
+    const respuesta = await fetch(`https://api.themoviedb.org/3/${type}/${id}?api_key=4c42277c85a8a8f307d358420965071c&language=es-ES`);
+    try {
+        const datos = await respuesta.json();
+
+        logoInfo.innerHTML = `
+        <div class="logoOverview">
+            <img class="mainLogo" src="https://image.tmdb.org/t/p/original/${logoMain}">
+            <p class="mainOverview">${datos.overview}</p>
+        </div>
+        <button id="btnPlay" class="reproducir">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg>
+            <span>Reproducir</span>
+        </button>
+        <button id="moreInfo" class="moreInfo">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="#fff"></path></svg>
+            <span>Más información</span>
+        </button>
+        `;
+        // ----- BACKDROP HERO -----
+        const mainBackdrop = document.getElementById('mainBackdrop');
+        const img = document.createElement('img');
+        img.classList.add('backdropHero');
+        img.setAttribute('src', `https://image.tmdb.org/t/p/original/${datos.backdrop_path}`)
+        mainBackdrop.appendChild(img);
+    
+        // ----- TRANSITION BACKDROP HERO -----
+        setTimeout(function(){
+            const backdrop = document.querySelector('.backdropHero');
+            backdrop.style.opacity = '0';
+            const mainLogo = document.querySelector('.mainLogo');
+            mainLogo.style.transform = 'scale(0.7) translate3d(0, 8vw, 0)';
+            const mainOverview = document.querySelector('.mainOverview');
+            mainOverview.style.transform = 'scale(0.0)';
+            mainOverview.style.opacity = '0';
+        }, 4500);
+    
+        // ----- MORE INFO -----
+        const moreInfo = document.getElementById('moreInfo');
+        moreInfo.addEventListener('click', () => {
+            getInfo(type, id)
+        })
+    } catch (error) {
+        console.log('Error', error);
+    }
+}
+mainInfo("tv", "209167");
+
 // ----- YOUTUBE API -----
 var tag = document.createElement('script');
 
@@ -181,7 +233,7 @@ function onPlayerStateChange(event) {
                 const backdrop = document.querySelector('.backdropHero');
                 backdrop.style.opacity = '0';
                 const mainLogo = document.querySelector('.mainLogo');
-                mainLogo.style.transform = 'scale(0.7) translate3d(0, 120px, 0)';
+                mainLogo.style.transform = 'scale(0.7) translate3d(0, 8vw, 0)';
                 const mainOverview = document.querySelector('.mainOverview');
                 mainOverview.style.transform = 'scale(0.0)';
                 mainOverview.style.opacity = '0';
@@ -203,55 +255,3 @@ function onPlayerStateChange(event) {
         mainOverview.style.opacity = '1';
     },120000)
 }
-
-// ---------- GET INFO HERO ----------
-const mainInfo = async(type, id) => {
-    const logoInfo = document.getElementById('logoInfo');
-    const logoMain = await getLogos(type, id);
-
-    const respuesta = await fetch(`https://api.themoviedb.org/3/${type}/${id}?api_key=4c42277c85a8a8f307d358420965071c&language=es-ES`);
-    try {
-        const datos = await respuesta.json();
-
-        logoInfo.innerHTML = `
-        <div class="logoOverview">
-            <img class="mainLogo" src="https://image.tmdb.org/t/p/original/${logoMain}">
-            <p class="mainOverview">${datos.overview}</p>
-        </div>
-        <button id="btnPlay" class="reproducir">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg>
-            <span>Reproducir</span>
-        </button>
-        <button id="moreInfo" class="moreInfo">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="#fff"></path></svg>
-            <span>Más información</span>
-        </button>
-        `;
-        // ----- BACKDROP HERO -----
-        const mainBackdrop = document.getElementById('mainBackdrop');
-        const img = document.createElement('img');
-        img.classList.add('backdropHero');
-        img.setAttribute('src', `https://image.tmdb.org/t/p/original/${datos.backdrop_path}`)
-        mainBackdrop.appendChild(img);
-    
-        // ----- TRANSITION BACKDROP HERO -----
-        setTimeout(function(){
-            const backdrop = document.querySelector('.backdropHero');
-            backdrop.style.opacity = '0';
-            const mainLogo = document.querySelector('.mainLogo');
-            mainLogo.style.transform = 'scale(0.7) translate3d(0, 120px, 0)';
-            const mainOverview = document.querySelector('.mainOverview');
-            mainOverview.style.transform = 'scale(0.0)';
-            mainOverview.style.opacity = '0';
-        }, 4500);
-    
-        // ----- MORE INFO -----
-        const moreInfo = document.getElementById('moreInfo');
-        moreInfo.addEventListener('click', () => {
-            getInfo(type, id)
-        })
-    } catch (error) {
-        console.log('Error', error);
-    }
-}
-mainInfo("tv", "209167");
