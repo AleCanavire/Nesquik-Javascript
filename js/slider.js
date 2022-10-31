b6e66a03525108eeb8f13fdbcf2a611b75b6bc22
AleCanavire <ale.canavire1@gmail.com>
2022-10-31 15:57:23 -0300

    List button

--- js/slider.js
+++ js/slider.js
@@ -102,6 +102,7 @@ const getSimilar = async(type, id) => {
         const moviesResults = filter.slice(0, 9);
 
         const similarTitles = document.getElementById('similarTitles');
+        let i = 0;
         let titles = "";
         moviesResults.forEach(async title => {
             let logo = await getLogos(type, title.id)
@@ -113,7 +114,7 @@ const getSimilar = async(type, id) => {
             if (logo !== undefined) {
                 titles += `
                 <div id="${title.id}" class="titleCard">
-                    <div class="poster">
+                    <div data-id="${title.id}" class="poster">
                         <img class="titleImg" src="https://image.tmdb.org/t/p/w300/${title.backdrop_path}">
                         <div class="titleLogo">
                             <img src="https://image.tmdb.org/t/p/w154/${logo}">
@@ -121,48 +122,86 @@ const getSimilar = async(type, id) => {
                         <svg class="play" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="titleCard-playSVG"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg>
                     </div>
                     <div class="titleMeta">
-                        <div class="metaL">
+                        <div data-id="${title.id}" class="metaL">
                             <span class="forYou">${forYou} % para ti</span>
                             <span class="age">18+</span>
                             <span class="year">${date.slice(0, 4)}</span>
                         </div>
-                        <div class="metaR">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 2V11H2V13H11V22H13V13H22V11H13V2H11Z" fill="currentColor"></path></svg>
+                        <div data-image="add" class="metaR">
+                            <img class="metaImg" data-image="add" src="../img/add.svg">
                         </div>
                     </div>
-                    <div class="overview">
+                    <div data-id="${title.id}" class="overview">
                         <p>${title.overview}</p>
                     </div>
                 </div>`
             } else{
                 titles += `
                 <div id="${title.id}" class="titleCard">
-                    <div class="poster">
+                    <div data-id="${title.id}" class="poster">
                         <img class="titleImg" src="https://image.tmdb.org/t/p/w300/${title.backdrop_path}">
                         <svg class="play" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="titleCard-playSVG"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg>
                     </div>
                     <div class="titleMeta">
-                        <div class="metaL">
+                        <div data-id="${title.id}" class="metaL">
                             <span class="forYou">${forYou} % para ti</span>
                             <span class="age">18+</span>
                             <span class="year">${date.slice(0, 4)}</span>
                         </div>
-                        <div class="metaR">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 2V11H2V13H11V22H13V13H22V11H13V2H11Z" fill="currentColor"></path></svg>
+                        <div data-image="add" class="metaR">
+                            <img class="metaImg" data-image="add" src="../img/add.svg">
                         </div>
                     </div>
-                    <div class="overview">
+                    <div data-id="${title.id}" class="overview">
                         <p>${title.overview}</p>
                     </div>
                 </div>`
             }
             similarTitles.innerHTML = titles;
+            i++
         });
-        $(similarTitles).on('click', '.titleCard', function (e){
-            const id = $(e.currentTarget).attr("id");
+
+        $(similarTitles).on('click', '.poster', function (e){
+            const id = $(e.currentTarget).attr("data-id");
             getInfo(type, id);
-            console.clear();
+            // console.clear();
         });
+
+        $(similarTitles).on('click', '.metaL', function (e){
+            const id = $(e.currentTarget).attr("data-id");
+            getInfo(type, id);
+            // console.clear();
+        });
+
+        $(similarTitles).on('click', '.overview', function (e){
+            const id = $(e.currentTarget).attr("data-id");
+            getInfo(type, id);
+            // console.clear();
+        });
+
+        $(similarTitles).on('click', '.metaImg', function (e){
+            const id = $(e.currentTarget).attr("data-image");
+            if (id == "add") {
+                this.src = "../img/added.svg";
+                this.dataset.image = "added";
+            }else if (id == "added") {
+                this.src = "../img/add.svg";
+                this.dataset.image = "add";
+            }
+        });
+
+        // const miLista = document.querySelector('.miLista');
+        // const imgList = document.querySelector('.imgList');
+        // miLista.addEventListener('click', ()=>{
+        //     if (imgList.dataset.image == "add") {
+        //         imgList.src = "../img/added.svg";
+        //         imgList.dataset.image = "added";
+        //         return
+        //     }else if (imgList.dataset.image == "added") {
+        //         imgList.src = "../img/add.svg";
+        //         imgList.dataset.image = "add";
+        //     }
+        // })
     } catch (error) {
         console.log('Error', error);
     }
@@ -225,7 +264,7 @@ const getInfo = async(type, id) => {
                         <span>Reproducir</span>
                     </button>
                     <button class="miLista">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 2V11H2V13H11V22H13V13H22V11H13V2H11Z" fill="#fff"></path></svg>
+                        <img class="imgList" data-image="add" src="../img/add.svg">
                     </button>
                     <button class="rate">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.696 8.7732C10.8947 8.45534 11 8.08804 11 7.7132V4H11.8377C12.7152 4 13.4285 4.55292 13.6073 5.31126C13.8233 6.22758 14 7.22716 14 8C14 8.58478 13.8976 9.1919 13.7536 9.75039L13.4315 11H14.7219H17.5C18.3284 11 19 11.6716 19 12.5C19 12.5929 18.9917 12.6831 18.976 12.7699L18.8955 13.2149L19.1764 13.5692C19.3794 13.8252 19.5 14.1471 19.5 14.5C19.5 14.8529 19.3794 15.1748 19.1764 15.4308L18.8955 15.7851L18.976 16.2301C18.9917 16.317 19 16.4071 19 16.5C19 16.9901 18.766 17.4253 18.3994 17.7006L18 18.0006L18 18.5001C17.9999 19.3285 17.3284 20 16.5 20H14H13H12.6228C11.6554 20 10.6944 19.844 9.77673 19.5382L8.28366 19.0405C7.22457 18.6874 6.11617 18.5051 5 18.5001V13.7543L7.03558 13.1727C7.74927 12.9688 8.36203 12.5076 8.75542 11.8781L10.696 8.7732ZM10.5 2C9.67157 2 9 2.67157 9 3.5V7.7132L7.05942 10.8181C6.92829 11.0279 6.72404 11.1817 6.48614 11.2497L4.45056 11.8313C3.59195 12.0766 3 12.8613 3 13.7543V18.5468C3 19.6255 3.87447 20.5 4.95319 20.5C5.87021 20.5 6.78124 20.6478 7.65121 20.9378L9.14427 21.4355C10.2659 21.8094 11.4405 22 12.6228 22H13H14H16.5C18.2692 22 19.7319 20.6873 19.967 18.9827C20.6039 18.3496 21 17.4709 21 16.5C21 16.4369 20.9983 16.3742 20.995 16.3118C21.3153 15.783 21.5 15.1622 21.5 14.5C21.5 13.8378 21.3153 13.217 20.995 12.6883C20.9983 12.6258 21 12.5631 21 12.5C21 10.567 19.433 9 17.5 9H15.9338C15.9752 8.6755 16 8.33974 16 8C16 6.98865 15.7788 5.80611 15.5539 4.85235C15.1401 3.09702 13.5428 2 11.8377 2H10.5Z" fill="#fff"></path></svg>
@@ -292,6 +331,9 @@ const getInfo = async(type, id) => {
                     </button>
                     <button class="miLista">
                         <img class="imgList" data-image="add" src="../img/add.svg">
+                        <div class="listText">
+                            <span>Agregar a Mi lista</span>
+                        </div>
                     </button>
                     <button class="rate">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.696 8.7732C10.8947 8.45534 11 8.08804 11 7.7132V4H11.8377C12.7152 4 13.4285 4.55292 13.6073 5.31126C13.8233 6.22758 14 7.22716 14 8C14 8.58478 13.8976 9.1919 13.7536 9.75039L13.4315 11H14.7219H17.5C18.3284 11 19 11.6716 19 12.5C19 12.5929 18.9917 12.6831 18.976 12.7699L18.8955 13.2149L19.1764 13.5692C19.3794 13.8252 19.5 14.1471 19.5 14.5C19.5 14.8529 19.3794 15.1748 19.1764 15.4308L18.8955 15.7851L18.976 16.2301C18.9917 16.317 19 16.4071 19 16.5C19 16.9901 18.766 17.4253 18.3994 17.7006L18 18.0006L18 18.5001C17.9999 19.3285 17.3284 20 16.5 20H14H13H12.6228C11.6554 20 10.6944 19.844 9.77673 19.5382L8.28366 19.0405C7.22457 18.6874 6.11617 18.5051 5 18.5001V13.7543L7.03558 13.1727C7.74927 12.9688 8.36203 12.5076 8.75542 11.8781L10.696 8.7732ZM10.5 2C9.67157 2 9 2.67157 9 3.5V7.7132L7.05942 10.8181C6.92829 11.0279 6.72404 11.1817 6.48614 11.2497L4.45056 11.8313C3.59195 12.0766 3 12.8613 3 13.7543V18.5468C3 19.6255 3.87447 20.5 4.95319 20.5C5.87021 20.5 6.78124 20.6478 7.65121 20.9378L9.14427 21.4355C10.2659 21.8094 11.4405 22 12.6228 22H13H14H16.5C18.2692 22 19.7319 20.6873 19.967 18.9827C20.6039 18.3496 21 17.4709 21 16.5C21 16.4369 20.9983 16.3742 20.995 16.3118C21.3153 15.783 21.5 15.1622 21.5 14.5C21.5 13.8378 21.3153 13.217 20.995 12.6883C20.9983 12.6258 21 12.5631 21 12.5C21 10.567 19.433 9 17.5 9H15.9338C15.9752 8.6755 16 8.33974 16 8C16 6.98865 15.7788 5.80611 15.5539 4.85235C15.1401 3.09702 13.5428 2 11.8377 2H10.5Z" fill="#fff"></path></svg>
@@ -350,14 +392,16 @@ const getInfo = async(type, id) => {
         }
         const miLista = document.querySelector('.miLista');
         const imgList = document.querySelector('.imgList');
+        const listText = document.querySelector('.listText span')
         miLista.addEventListener('click', ()=>{
             if (imgList.dataset.image == "add") {
                 imgList.src = "../img/added.svg";
                 imgList.dataset.image = "added";
-                return
+                listText.innerText = "Quitar de Mi lista";
             }else if (imgList.dataset.image == "added") {
                 imgList.src = "../img/add.svg";
                 imgList.dataset.image = "add";
+                listText.innerText = "Agregar a Mi lista";
             }
         });
     } catch (error) {
@@ -391,7 +435,7 @@ const cargarPeliculas = async(type, movies, movies2, title, nombre, num, contain
             movieInfo.style.opacity = "1";
             movieInfo.style.transform = "scale(1.0)";
             bgInfo.style.opacity = "1";
-            console.clear();
+            // console.clear();
         });
     
         // ---------- EXIT CARD INFO ----------
@@ -410,7 +454,7 @@ const cargarPeliculas = async(type, movies, movies2, title, nombre, num, contain
                 }
                 const titlePage = document.getElementById('titlePage');
                 titlePage.innerText = "Home - Nesquik";
-                console.clear();
+                // console.clear();
             }
         });
     
